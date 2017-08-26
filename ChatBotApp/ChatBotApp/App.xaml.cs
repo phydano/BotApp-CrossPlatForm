13503a45a65f987e193db9a2a46eb1f91385eeb8
Dano Phy <ph.dano@gmail.com>
2017-08-27 11:49:14 +1200

    Add all files

---
+++ ChatBotApp/ChatBotApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace ChatBotApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.MainPage = new MainPage { Title = "ContosoBot" };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ChatBotApp/ChatBotApp/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace ChatBotApp
+{
+    public partial class MainPage : ContentPage
+    {
+        // A really simple app that embedded the bot webchat in. 
+        public MainPage()
+        {
+            var browser = new WebView();
+            browser.Source = "https://webchat.botframework.com/embed/Contoso_SupportBot?s=byiqpxSrboM.cwA.MR0.FQ-fQiM-AxaEp5zWRtNWNKiuZAgn08aKNssVXDtbWoc";
+            this.Content = browser;
+        }
+    }
+}
