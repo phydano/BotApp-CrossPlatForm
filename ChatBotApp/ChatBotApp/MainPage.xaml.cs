06cb7062d440ee774e2c380675070de6568da7c3
Dano Phy <ph.dano@gmail.com>
2017-09-02 12:08:16 +1200

    add a button

--- ChatBotApp/ChatBotApp/MainPage.xaml.cs
+++ ChatBotApp/ChatBotApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace ChatBotApp
 {
@@ -6,6 +7,12 @@ namespace ChatBotApp
     {
         // A really simple app that embedded the bot webchat in. 
         public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Generate a new web view when the user click on starting the chat 
+        public async void ButtonIsClicked(object sender, EventArgs e)
         {
             var browser = new WebView();
             browser.Source = "https://webchat.botframework.com/embed/Contoso_SupportBot?s=byiqpxSrboM.cwA.MR0.FQ-fQiM-AxaEp5zWRtNWNKiuZAgn08aKNssVXDtbWoc";
--- ChatBotApp/ChatBotApp/obj/Debug/ChatBotApp.MainPage.xaml.g.cs
+++ ChatBotApp/ChatBotApp/obj/Debug/ChatBotApp.MainPage.xaml.g.cs
@@ -11,8 +11,8 @@
 namespace ChatBotApp {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("c:\\users\\dano\\documents\\visual studio 2017\\Projects\\ChatBotApp\\ChatBotApp\\ChatBot" +
-        "App\\MainPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\Dano\\Documents\\Github\\BotApp-CrossPlatForm\\ChatBotApp\\ChatBotApp\\MainPag" +
+        "e.xaml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
